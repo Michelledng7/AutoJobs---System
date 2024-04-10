a051a2f204be012f4d62fa5924f4e2abcd0c8886
Michelle <michelledng7@gmail.com>
2024-04-10 16:18:50 +1000

    first commit

---
+++ showuptime.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+showuptime(){
+	up=$(uptime | cut -c4-)
+	since=$(uptime)
+	cat << EOF
+-----
+This is michelle has been up for ${up}
+It has been running since ${since}
+------
+EOF
+}
+showuptime 
+
